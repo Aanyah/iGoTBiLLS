021f43f6a2e230a869dbe29dfebd59fcb12ec6bf
CRUMBB <aanyahlc@gmail.com>
2022-03-26 16:27:52 -0400

    added manifest/service-worker/etc/code refractor to fix mongodb driver

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
@@ -18,12 +18,13 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!--😜😜😜😜😜😜😜😜😜`);
 });
